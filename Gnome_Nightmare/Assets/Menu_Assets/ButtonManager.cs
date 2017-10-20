6299d5196b596382482b792e40c7fa41db5623bb
jukejake <jacob.rosengren@uoit.net>
2017-10-20 16:13:11 -0400

    Added the ability to Load different Scenes
    Added the ability to Load different Scenes when the single player button is clicked

--- Gnome_Nightmare/Assets/Menu_Assets/ButtonManager.cs
+++ Gnome_Nightmare/Assets/Menu_Assets/ButtonManager.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour {
-
-    public void SinglePlayerButton(string newGame) { SceneManager.LoadScene(newGame); }
+    
+    public void SinglePlayerButton(string newGame) { /*SceneManager.LoadScene(newGame);*/ }
     public void MultiPlayerButton() { }
     public void SettingsButton() { }
     public void CreditButton() { }
---
+++ Gnome_Nightmare/Assets/Menu_Assets/LoadScenes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour {
+
+    public void LoadSceneFromString(string newGame) { SceneManager.LoadScene(newGame); }
+
+    public void LoadSceneFromDropDown(GameObject dropDown) {
+        int SceneNum = dropDown.GetComponent<Dropdown>().value;
+        //Debug.Log(dropDown.GetComponent<Dropdown>().options[SceneNum].text);
+        SceneManager.LoadScene(dropDown.GetComponent<Dropdown>().options[SceneNum].text);
+    }
+
+    public void LoadScenesToDropDown(GameObject dropDown) {
+        dropDown.GetComponent<Dropdown>().ClearOptions();
+        List<string> SceneNames = new List<string>();
+        //Debug.Log(SceneManager.sceneCountInBuildSettings);
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++){
+            var path = SceneUtility.GetScenePathByBuildIndex(i);
+            var sceneName = path.Substring(0, path.Length - 6).Substring(path.LastIndexOf('/') + 1);
+            if (sceneName == "Menu_Start") { }
+            else { SceneNames.Add(sceneName); }
+            
+            //Debug.Log(sceneName);
+        }
+        dropDown.GetComponent<Dropdown>().AddOptions(SceneNames);
+    }
+}
