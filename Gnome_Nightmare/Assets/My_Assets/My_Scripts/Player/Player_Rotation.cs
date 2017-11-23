ff5ef8fe42a1a18a5fa24a151871a53fc5b98893
Matthew Dupuis <100601589@oncampus.local>
2017-11-23 18:54:47 -0500

    Jacky follows camera now (Sort of)

---
+++ Gnome_Nightmare/Assets/My_Assets/My_Scripts/Player/Player_Rotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Rotation : MonoBehaviour {
+
+    public Transform bone;
+    public Transform cameraTarget;
+
+    void LateUpdate()
+    {
+        bone.transform.LookAt(cameraTarget.transform.forward);
+    }
+}
