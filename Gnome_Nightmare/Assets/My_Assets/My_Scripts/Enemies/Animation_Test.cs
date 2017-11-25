cf8ee8e6381a29a947f666d3634cd6e6e2e889e8
Matthew Dupuis <matthew.dupuis@uoit.net>
2017-11-25 18:19:30 -0500

    Gnome Animations in!
    Still need to be attached to the gnomes that spawn via spawners.

---
+++ Gnome_Nightmare/Assets/My_Assets/My_Scripts/Enemies/Animation_Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation_Test : MonoBehaviour {
+
+    public Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("1"))
+            anim.Play("Gnome_Hit",-1,0f);
+        else if (Input.GetKeyDown("2"))
+            anim.Play("Gnome_Die", -1, 0f);
+    }
+}
