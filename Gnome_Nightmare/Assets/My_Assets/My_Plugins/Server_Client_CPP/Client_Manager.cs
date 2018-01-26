d75f6458a3e449722c1cf1ac953cc79e68d5c68c
jukejake <jacob.rosengren@uoit.net>
2018-01-26 12:57:30 -0500

    Adjusted the Server/client system
    Adjusted the Server/client system
    also the fire

---
+++ Gnome_Nightmare/Assets/My_Assets/My_Plugins/Server_Client_CPP/Client_Manager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+
+public class Client_Manager : MonoBehaviour {
+
+    [DllImport("Client")]
+    public static extern void InitAccess();
+    [DllImport("Client")]
+    public static extern void DestroyManager();
+    [DllImport("Client")]
+    public static extern bool Initialize();
+    [DllImport("Client")]
+    public static extern void UpdateClient();
+    [DllImport("Client")]
+    public static extern IntPtr GetServerIP();
+    [DllImport("Client")]
+    public static extern bool CheckServerIP(string s_IP); //char * data
+    [DllImport("Client")]
+    public static extern bool SendData(string data); //const char * data
+    [DllImport("Client")]
+    public static extern IntPtr ReceiveData();
+
+
+    private bool Initialized = false;
+    private bool Connected = false;
+    public string IP = "192.168.0.1";
+    public string SentMessage = "Hello!";
+    public string ReceivedMessage = "";
+
+    // Use this for initialization
+    void Start () {
+        InitAccess();
+        if (Initialize()) {
+            Initialized = true;
+            if (CheckServerIP(IP)) {
+                Connected = true;
+                SendData(SentMessage);
+                ReceivedMessage = Marshal.PtrToStringAnsi(ReceiveData());
+                Debug.Log(ReceivedMessage);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Initialized && !Connected) { CheckServerIP(IP); }
+        else {
+            //SendData(SentMessage);
+            //ReceivedMessage = Marshal.PtrToStringAnsi(ReceiveData());
+        }
+	}
+}
---
+++ Gnome_Nightmare/Assets/My_Assets/My_Plugins/Server_Client_CPP/Sever_Manager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+
+public class Sever_Manager : MonoBehaviour {
+
+
+    [DllImport("Server")]
+    public static extern void InitAccess();
+    [DllImport("Server")]
+    public static extern void DestroyManager();
+    [DllImport("Server")]
+    public static extern bool Initialize();
+    [DllImport("Server")]
+    public static extern void UpdateServer();
+    [DllImport("Server")]
+    public static extern bool SetPortNumber(int port);
+    [DllImport("Server")]
+    public static extern bool LookForActivity();
+    [DllImport("Server")]
+    public static extern bool NewConnection();
+    [DllImport("Server")]
+    public static extern IntPtr ReceiveData();
+    [DllImport("Server")]
+    public static extern IntPtr ReturnLastAddress();
+
+    //private
+    public bool Initialized = false;
+    public bool PortSet = false;
+    public int Port = 8888;
+    public string SentMessage = "Hello!";
+    public string ReceivedMessage = "";
+
+    // Use this for initialization
+    void Start () {
+        DestroyManager();
+        InitAccess();
+        if (Initialize()) { Initialized = true; Debug.Log("What"); }
+        //if (SetPortNumber(Port)) { PortSet = true; }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Initialized && LookForActivity()) {
+            Debug.Log("...");
+            if (NewConnection()) { Debug.Log("[Client has Connected]"); }
+            //ReceivedMessage = Marshal.PtrToStringAnsi(ReceiveData());
+            //if (ReceivedMessage != "") { Debug.Log(ReceivedMessage); }
+        }
+	}
+}
