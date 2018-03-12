52c52e16b5a46ecf3a373bda037f8b942898ab9f
jukejake <jacob.rosengren@uoit.com>
2018-03-12 15:20:22 -0400

    Fixed Player Isgrounded Logic
    ~Fixed Player Isgrounded Logic

--- Gnome_Nightmare/Assets/My_Assets/My_Scripts/Player/CheckCollider.cs
+++ Gnome_Nightmare/Assets/My_Assets/My_Scripts/Player/CheckCollider.cs
@@ -7,12 +7,13 @@ public class CheckCollider : MonoBehaviour {
     public bool IsTriggered = false;
 
     private void OnTriggerStay(Collider other) {
-        if ((other.gameObject.layer == 10 || other.gameObject.layer == 2)
-            || (other.gameObject.layer == 11 || other.gameObject.layer == 12))
-        { IsTriggered = false; }
-        else { IsTriggered = true; }
+        //Don't collide with
+        if (other.gameObject.layer == 2 || other.gameObject.layer == 10 || other.gameObject.layer == 11 || other.gameObject.layer == 12) { IsTriggered = false; }
+        //Collide with
+        else if (other.gameObject.layer == 0 || other.gameObject.layer == 4 || other.gameObject.layer == 8 ) { IsTriggered = true; }
     }
     private void OnTriggerExit(Collider other) {
-        IsTriggered = false;
+        //IsTriggered = false;
+        if (other.gameObject.layer == 0 || other.gameObject.layer == 4 || other.gameObject.layer == 8) { IsTriggered = false; }
     }
 }
