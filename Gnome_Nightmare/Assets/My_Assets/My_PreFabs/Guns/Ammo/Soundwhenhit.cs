d129dc50b60841027379e168e1c3ba95a4486cd7
jukejake <jacob.rosengren@uoit.net>
2018-04-05 17:15:22 -0400

    Cinematic Update
    Cinematic Update

---
+++ Gnome_Nightmare/Assets/My_Assets/My_PreFabs/Guns/Ammo/Soundwhenhit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soundwhenhit : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name == "CM vcam1" || other.gameObject.tag == "Cinemachine_Trigger") {
+            Debug.Log("Hit");
+            this.gameObject.GetComponent<AudioSource>().Play();
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.gameObject.name == "CM vcam1" || other.gameObject.tag == "Cinemachine_Trigger") {
+            //Debug.Log("Hit");
+            //this.gameObject.GetComponent<AudioSource>().Play();
+        }
+    }
+}
